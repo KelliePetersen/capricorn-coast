c3006065158505d8b34dad19b6c3eb11e089dc5f
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-08-19 19:41:24 -0400

    add grid hover effects and links

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,11 +1,9 @@
 import BackToTop from './modules/BackToTop';
 import MobileMenu from './modules/MobileMenu';
-import SelectGridItem from './modules/SelectGridItem';
 import WidescreenMenu from './modules/WidescreenMenu';
 import DynamicNav from './modules/DynamicNav';
 
 const backToTop = new BackToTop();
 const mobileMenu = new MobileMenu();
-const selectGridItem = new SelectGridItem();
 const widescreenMenu = new WidescreenMenu();
 const dynamicNav = new DynamicNav();
